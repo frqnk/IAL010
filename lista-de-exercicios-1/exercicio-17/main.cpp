75ca8b5b387b52a84454b395db5b9fd84bdd8e55
Frank <33494411+frqnk@users.noreply.github.com>
2023-06-05 04:19:17 -0300

    exercício 17 resolvido

--- lista-de-exercicios-1/exercicio-17/main.cpp
+++ lista-de-exercicios-1/exercicio-17/main.cpp
@@ -1,8 +1,37 @@
 #include <iostream>
+#include <locale>
 
 using namespace std;
 
+bool verifica_perfeito(int n) {
+    int aux = 0;
+    for(int i = n-1; i > 0; i--) {
+        if(n % i == 0) {
+            aux += i;
+        }
+    }
+    if(aux == n) {
+        return true;
+    }
+    else {
+        return false;
+    }
+}
+
 int main() {
+    setlocale(LC_ALL, "Portuguese");
+
+    int input;
+
+    cout << "Dê um número para verificar se é perfeito: ";
+    cin >> input;
+
+    if(verifica_perfeito(input) == true) {
+        cout << "Verdadeiro" << endl;
+    }
+    else {
+        cout << "Falso" << endl;
+    }
     system("pause");
 
     return 0;
