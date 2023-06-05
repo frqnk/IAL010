46a811bec150c173dc6a72bca08d4697ad7783fc
Frank <33494411+frqnk@users.noreply.github.com>
2023-06-05 01:59:21 -0300

    reinterpretação do exercício 9

--- lista-de-exercicios-1/exercicio-09/main.cpp
+++ lista-de-exercicios-1/exercicio-09/main.cpp
@@ -1,66 +1,66 @@
 #include <iostream>
-#include <locale>
-#include <cfloat>
+#include <cstdlib>
+#include <ctime>
 
 using namespace std;
 
 int main() {
-    setlocale(LC_ALL, "Portuguese");
+    srand(time(NULL));
 
-    int n;
-    long double soma_de_a = 0, soma_de_b = 0, produto_escalar = 0;
-
-    cout << "Dê o número de elementos dos vetores A e B: ";
-    cin >> n;
-
+    int n = rand() % 11 + 5; // de 5 a 15
     int a[n], b[n], c[n], d[n];
+    int soma_de_a = 0, soma_de_b = 0, produto_escalar = 0;
 
-    cout << "Dê os elementos do vetor A: ";
     for(int i = 0; i < n; i++) {
-        cin >> a[i];
-        soma_de_a += a[i];
-    }
-
-    cout << "Dê os elementos do vetor B: ";
-    for(int i = 0; i < n; i++) {
-        cin >> b[i];
-        soma_de_b += b[i];
-        produto_escalar += (long double) a[i] * b[i];
+        a[i] = rand() % 1000; // de 0 a 999
+        b[i] = rand() % 1000;
         c[i] = a[i] + b[i];
         d[i] = a[i] - b[i];
+        soma_de_a += a[i];
+        soma_de_b += b[i];
+        produto_escalar += a[i] * b[i];
     }
 
-    cout << "Resultados:" << endl;
-
-    cout << "A[" << n << "] = {";
+    cout << "A[" << n << "] = {\t";
     for(int i = 0; i < n; i++) {
         cout << a[i];
-        if(i != n-1) cout << ", ";
+        if(i != n-1) {
+            cout << ",";
+        }
+        cout << "\t";
     }
     cout << "}" << endl;
 
-    cout << "B[" << n << "] = {";
+    cout << "B[" << n << "] = {\t";
     for(int i = 0; i < n; i++) {
         cout << b[i];
-        if(i != n-1) cout << ", ";
+        if(i != n-1) {
+            cout << ",";
+        }
+        cout << "\t";
     }
     cout << "}" << endl;
 
-    cout << "C[" << n << "] = {";
+    cout << "C[" << n << "] = {\t";
     for(int i = 0; i < n; i++) {
         cout << c[i];
-        if(i != n-1) cout << ", ";
+        if(i != n-1) {
+            cout << ",";
+        }
+        cout << "\t";
     }
     cout << "}" << endl;
 
-    cout << "D[" << n << "] = {";
+    cout << "D[" << n << "] = {\t";
     for(int i = 0; i < n; i++) {
         cout << d[i];
-        if(i != n-1) cout << ", ";
+        if(i != n-1) {
+            cout << ",";
+        }
+        cout << "\t";
     }
     cout << "}" << endl;
 
-    cout.precision(LDBL_DIG);
     cout << "Soma dos elementos de A = " << soma_de_a << endl;
     cout << "Soma dos elementos de B = " << soma_de_b << endl;
     cout << "Produto escalar de A por B = " << produto_escalar << endl;
