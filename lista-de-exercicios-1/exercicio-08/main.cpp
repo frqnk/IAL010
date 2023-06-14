869221fc8edf7334da5d10bb0c536e9c55836daf
Frank <33494411+frqnk@users.noreply.github.com>
2023-06-14 03:06:53 -0300

    otimização do código e explicação do resultado

--- lista-de-exercicios-1/exercicio-08/main.cpp
+++ lista-de-exercicios-1/exercicio-08/main.cpp
@@ -1,20 +1,34 @@
+/*
+Soma de P.G. = a_1(q^n-1)/(q-1)
+
+Onde:
+
+a_1, a_2, a_3, a_4, ... = 1, 2, 4, 8, ...
+
+q = a_n/a_(n-1) = 2
+
+n = 64
+
+Portanto:
+
+S = 1(2^64-1)/(2-1) = 2^64-1 = 18.446.744.073.709.551.615
+
+Curiosamente esse resultado é igual a ULLONG_MAX
+*/
+
 #include <iostream>
 
 using namespace std;
 
 int main() {
     unsigned long long int graos_no_quadro, graos_no_tabuleiro;
-    // curiosamente o resultado deste exercício é igual a ULLONG_MAX
 
-    for(int quadro = 1; quadro <= 64; quadro++) {
-        if(quadro == 1) {
-            graos_no_quadro = 1;
-            graos_no_tabuleiro = graos_no_quadro;
-        }
-        else {
-            graos_no_quadro *= 2;
-            graos_no_tabuleiro += graos_no_quadro;
-        }
+    graos_no_quadro = 1;
+    graos_no_tabuleiro = graos_no_quadro;
+
+    for(int quadro = 2; quadro <= 64; quadro++) {
+        graos_no_quadro *= 2;
+        graos_no_tabuleiro += graos_no_quadro;
     }
 
     cout << graos_no_tabuleiro << endl;
