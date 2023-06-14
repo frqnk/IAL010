5e8679dce55336802359160f028e8a48e09a1426
Frank <33494411+frqnk@users.noreply.github.com>
2023-06-14 11:13:52 -0300

    algoritmo de ordenação alterado para ICan'tBelieveItCanSort

--- lista-de-exercicios-1/exercicio-10/main.cpp
+++ lista-de-exercicios-1/exercicio-10/main.cpp
@@ -10,6 +10,7 @@ int main() {
 
     cout << "Dê o número de elementos do vetor: ";
     cin >> n;
+
     int vetor[n];
 
     cout << "Dê os elementos do vetor: ";
@@ -17,10 +18,11 @@ int main() {
         cin >> vetor[i];
     }
 
-    for(int i = 0; i < n - 1; i++) {
-        for(int j = i + 1; j < n; j++) {
-            if(vetor[j] < vetor[i]) {
-                swap(vetor[j], vetor[i]);
+    // "ICan'tBelieveItCanSort", afinal, por que não?
+    for(int i = 0; i < n; i++) {
+        for(int j = 0; j < n; j++) {
+            if(vetor[i] < vetor[j]) {
+                swap(vetor[i], vetor[j]);
             }
         }
     }
